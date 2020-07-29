7168bee259b348ed26d4ed9bf3d1bcc754cc6e13
Atula Demon <caikinhdenlc@gmail.com>
2020-07-30 00:44:34 +0700

    Rin update front-end

--- app/src/main/java/com/example/krid/ui/AccountDetailFragment.java
+++ app/src/main/java/com/example/krid/ui/AccountDetailFragment.java
@@ -7,10 +7,7 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
 
 import com.example.krid.R;
 
@@ -18,7 +15,7 @@ public class AccountDetailFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
-        View root = inflater.inflate(R.layout.fragment_account_detail, container, false);
+        View root = inflater.inflate(R.layout.fragment_account_detail_adv, container, false);
         final TextView textView = root.findViewById(R.id.text_account_detail);
 
         return root;
