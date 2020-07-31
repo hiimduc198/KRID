27b59af9d606cb27c3749c2cd16cc4ab0e5b876e
ducpq <poseidon121995@gmail.com>
2020-07-31 11:35:43 +0700

    recommit advertiser detail fragment

--- app/src/main/java/com/example/krid/ui/CreateCampaignFragment2.java
+++ app/src/main/java/com/example/krid/ui/CreateCampaignFragment2.java
@@ -10,16 +10,33 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 import com.example.krid.R;
+import com.example.krid.database.CampaignDao;
 import com.example.krid.model.Campaign;
 
 
 public class CreateCampaignFragment2 extends Fragment {
-    private Button btnNext;
+    private Button btnDone;
     private EditText etDescript;
-    private CheckBox cbSendP;
-    private CheckBox cbSendL;
+    private EditText edReward;
+    private EditText txt_cash;
+    private EditText txt_hashtag;
+    private EditText txt_work;
+    private EditText ed_anotherinfo;
+    private EditText txt_website;
+    private EditText txt_age_range;
+    private RadioButton checkBox_allsex;
+    private RadioButton checkBox_female;
+    private RadioButton checkBox_male;
+    private CheckBox checkBox6;
+    private CheckBox checkBox5;
+    private CheckBox checkBox4;
+    private CheckBox checkBox3;
+    private CheckBox checkBox9;
+    private CheckBox checkBox8;
     private Campaign campaign;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -27,18 +44,35 @@ public class CreateCampaignFragment2 extends Fragment {
         campaign = (Campaign)getArguments().getSerializable("param");
         // Inflate the layout for this fragment
         View root =  inflater.inflate(R.layout.create_campaign_02, container, false);
-//        btnNext = root.findViewById(R.id.btn_next_creatcampaign02);
+        btnDone = root.findViewById(R.id.btn_createcampaign_next);
         etDescript = root.findViewById(R.id.txt_description_creatcampaign);
-//        cbSendP = root.findViewById(R.id.cbx_sendgoods_createcampaign);
-//        cbSendL = root.findViewById(R.id.cbx_checkin_createcampaign);
-        btnNext.setOnClickListener(new View.OnClickListener() {
+        checkBox6  = root.findViewById(R.id.checkBox6);
+        checkBox5  = root.findViewById(R.id.checkBox5);
+        checkBox4  = root.findViewById(R.id.checkBox4);
+        checkBox3  = root.findViewById(R.id.checkBox3);
+        checkBox9  = root.findViewById(R.id.checkBox9);
+        checkBox8  = root.findViewById(R.id.checkBox8);
+        edReward  = root.findViewById(R.id.edReward);
+        txt_cash  = root.findViewById(R.id.txt_cash);
+        txt_hashtag  = root.findViewById(R.id.txt_hashtag);
+        txt_work  = root.findViewById(R.id.txt_work);
+        ed_anotherinfo  = root.findViewById(R.id.ed_anotherinfo);
+        txt_website  = root.findViewById(R.id.txt_website);
+        txt_age_range  = root.findViewById(R.id.txt_age_range);
+        checkBox_allsex  = root.findViewById(R.id.checkBox_allsex);
+        checkBox_female  = root.findViewById(R.id.checkBox_female);
+        checkBox_male  = root.findViewById(R.id.checkBox_male);
+
+
+        btnDone.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                campaign = new Campaign();
                 campaign.setDescription(etDescript.getText().toString().trim());
 
-                ((AdvertiserActivity) requireActivity()).navigateToFragmentWithArgs(new CreateCampaignFragment2(), campaign);
+                CampaignDao.addNewCampaign(campaign);
+                Toast.makeText(getContext(), "Register success.", Toast.LENGTH_LONG).show();
+
+                ((AdvertiserActivity) requireActivity()).navigateToFragmentWithoutArgs(new HomeFragment());
             }
         });
                 return root;
